7a8a43f7f8a33be18a192cdf2cf0d9cd6c5e739d
DemonMaike <dmitry.malyshevv@ya.ru>
2024-04-28 18:35:03 +0400

    Update whisper agent

--- whisper_agent.py
+++ whisper_agent.py
@@ -38,7 +38,7 @@ async def handle_task(session, file_path):
 
     try:
         # Отправляем задачу в контейнер, в дальнейшем должен быть хаб.
-        async with session.post('http://127.0.0.1:5000/transcribe', data=data, timeout=360) as response:
+        async with session.post('http://127.0.0.1:5000/transcribe', data=data, timeout=500) as response:
             if response.status == 200:
                 result = await response.json()
 
