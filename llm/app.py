953fdd7bcbf83ba54635466fd8a1943be0b83bce
DemonMaike <i@dmitrymalyshev.ru>
2024-05-30 11:09:39 +0000

    Update

--- llm/app.py
+++ llm/app.py
@@ -14,7 +14,7 @@ ALLOWED_EXTENSIONS = {"txt"}
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 device = "cuda"  # the device to load the model onto
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"
 
 model = AutoModelForCausalLM.from_pretrained(
     "mistralai/Mistral-7B-Instruct-v0.2", use_auth_token=HF_TOKEN, device_map="auto"
@@ -30,7 +30,7 @@ def analizing_meeting(prompt: str, content: str) -> str:
     encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
     model_inputs = encodeds.to(device)
 
-    generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+    generated_ids = model.generate(model_inputs, max_new_tokens=500, do_sample=True)
     decoded = tokenizer.batch_decode(generated_ids)
 
     return decoded[0]
@@ -50,7 +50,7 @@ def create_report():
     if file.filename == "":
         return jsonify({"error": "No selected file"}), 400
     if request.form.get("prompt") == "" or None:
-        return jsonify({"error": "No prompt"}), 400
+        prompt = "Выпиши главную цель совещания, определи имена людей и сопоставь спикеров и их роли, обсуждаемые вопросы подробно, конспект встречи, ключевые договоренности, сроки и ответственных по задачам. Ответь на русском языке."
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
@@ -61,10 +61,12 @@ def create_report():
                 file_content = file.read()
             result = analizing_meeting(prompt, file_content)
         except Exception as e:
+            print(e)
             return jsonify({"error": f"{e}"}), 400
         return jsonify(report=result, filename=filename.split(".")[0])
     else:
         return jsonify({"error": "Invalid file type"}), 400
+    torch.cuda.empty_cache()
 
 
 if __name__ == "__main__":
